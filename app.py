405b61f982b6a98a9da171ecc555fa99f77a3f6a
Lokaka <monishine_code@163.com>
2024-10-04 22:22:00 +0800

    feat: Flask 基本概念

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from datetime import datetime
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,6 +9,24 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/about')
+def about(): return 'I am about page'
+
+@app.route('/time')
+def get_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Page not found', 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return 'Internal server error', 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
